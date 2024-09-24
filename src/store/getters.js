9f1af33e6edb2567496c5b6ae6e8454925ea169b
shiny <1258553967@qq.com>
2024-09-24 23:35:27 +0800

    完成了vuex 和 相应拦截

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,7 @@
 const getters = {
   sidebar: state => state.app.sidebar,
-  device: state => state.app.device
-  // token: state => state.user.token,
+  device: state => state.app.device,
+  token: state => state.user.token // 在根级的getters上 开发子模块的属性给别人用
   // avatar: state => state.user.avatar,
   // name: state => state.user.name
 }
